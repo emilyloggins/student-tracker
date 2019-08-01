32bb1de9a958698bb938f905ee21250337631d50
Emily Loggins <em.loggins7@gmail.com>
2019-08-01 15:51:21 -0500

    part three complete

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -2,10 +2,8 @@ using System;
 
 namespace student_tracker
 {
-    class Instructor
+    class Instructor : NSSPerson
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string SlackHandle { get; set; }
         public int Cohort { get; set; }
         public string Specialty { get; set; }
