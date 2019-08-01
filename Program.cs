14fa2c8afb23844ba98e5936c3abee597d74df0e
Emily Loggins <em.loggins7@gmail.com>
2019-08-01 14:14:58 -0500

    part two

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace student_tracker
 {
@@ -100,6 +101,60 @@ namespace student_tracker
             instructorTwo.AssignExercise(dictionaries, studentThree);
             instructorThree.AssignExercise(routing, studentOne);
             instructorTwo.AssignExercise(interfaces, studentThree);
+
+            List<Instructor> instructors = new List<Instructor>()
+            {
+                instructorOne,
+                instructorTwo,
+                instructorThree
+            };
+
+            List<Cohort> cohorts = new List<Cohort>()
+            {
+                twentyNine,
+                thirtyOne,
+                thirtyTwo
+            };
+
+            // List exercises for the JavaScript language by using the Where() LINQ method.
+            IEnumerable<Exercise> JavascriptExercises =
+            from exercise in exercises
+            where exercise.Language == "Javascript"
+            select exercise;
+
+            // List students in a particular cohort by using the Where() LINQ method.
+            IEnumerable<Student> CohortThirtyTwoStudents =
+            from student in students
+            where student.Cohort == 32
+            select student;
+
+            // List instructors in a particular cohort by using the Where() LINQ method.
+            IEnumerable<Instructor> CohortThirtyTwoInstructors =
+            from instructor in instructors
+            where instructor.Cohort == 32
+            select instructor;
+
+            // Sort the students by their last name.
+            IEnumerable<Student> AscendingNames = 
+            from student in students
+            orderby student.LastName
+            select student;
+
+            // Display any students that aren't working on any exercises (Make sure one of your student instances don't have any exercises. Create a new student if you need to.)
+            Student student4 = new Student
+            {
+                FirstName = "Addam",
+                LastName = "Joor",
+                SlackHandle = "ajoor",
+                Cohort = 32
+            };
+            Student student5 = new Student 
+            {
+                FirstName = "Abby",
+                LastName = "Loggins",
+                SlackHandle = "aloggins",
+                Cohort = 29
+            };
         }
     }
 }
