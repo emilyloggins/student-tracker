c08dafce2482507f6108a747e4b467d1fa2632ee
Emily Loggins <em.loggins7@gmail.com>
2019-08-02 14:31:14 -0500

    formatting

--- Models/NSSPerson.cs
+++ Models/NSSPerson.cs
@@ -1,6 +1,7 @@
 
 namespace student_tracker
 {
+    // Creating a base class for all people at NSS. Instructor and Student will inherit.
     public class NSSPerson
     {
         public string FirstName { get; set; }
